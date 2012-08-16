9152ce12e2524b7ffa5d33d88f8525906deb7430
Jeffrey de Wit <Jeffrey.deWit@gmail.com>
2012-08-16 11:50:47 -0400

    changed home content
    Changed the static blocks to show WP page content of "Front page"
    instead.

--- index.php
+++ index.php
@@ -30,33 +30,16 @@ get_header(); ?>
 						</article><!-- #post-<?php the_ID(); ?> -->
 					<?php endwhile; ?>
 				</div>
-				<div class="home-blocks">
-					<div class="about-me home-block">
-					<?php if (qtrans_getLanguage() == 'en') : ?>
-						<h3>Who am I?</h3>
-						<p>I am a front-end/web developer from the Netherlands that is currently living in the US. 
-						I've a peculiar set of interests that will put me in the realm of geekdom.</p>
-						<a href="/about/">Read more about me</a>
-					<?php else : ?>
-						<h3>Wie ben ik?</h3>
-						<p>Ik ben een front-end/web developer uit Nederland wie op dit moment in de VS woont.
-						Ik heb een ongebruikelijke set interesses die me vrij duidelijk in het domein van "geek" plaatsen.</p>
-						<a href="/about/">Lees meer over mij</a>
-					<?php endif; ?>
-					</div>
-					<div class="contact home-block">
-						<?php if (qtrans_getLanguage() == 'en') : ?>
-							<h3>Want to contact me?</h3>
-							<p>The easiest, and fastest, way to reach me is probably <a href="http://twitter.com/cheffheid">Twitter</a>. 
-							If you want to say more than a simple "Hi!" and express yourself in more than 140 characters, e-mail me at Jeffrey.deWit at gmail.com.</p>
-						<?php else : ?>
-							<h3>Wil je me bereiken?</h3>
-							<p>De makkelijkste, en snelste, manier is waarschijnlijk via <a href="http://twitter.com/cheffheid">Twitter</a>. 
-							Als je meer dan alleen een simpele "Hallo!" wilt zeggen en je meer dan 140 karakters nodig hebt, mail me dan Jeffrey.deWit at gmail.com.</p>
-						<?php endif; ?>
-					</div>
-					<div class="clear"</div>
-				</div>
+				<?php 
+					
+					$frontpage_id = get_option('page_on_front');
+					$frontpage = get_page( $frontpage_id );
+					
+					$content = $frontpage->post_content;
+					$content = apply_filters('the_content', $content);
+					$content = str_replace(']]>', ']]>', $content);
+					echo $content;
+				?>
 			</div><!-- #content -->
 		</div><!-- #primary -->
 
