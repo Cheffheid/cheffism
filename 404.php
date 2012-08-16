c8ba355a309276a3b2362fe9484cf8086b376da3
Jeffrey de Wit <Jeffrey.deWit@gmail.com>
2012-08-16 12:53:55 -0400

    Changed 404 to be more like archives

--- 404.php
+++ 404.php
@@ -11,29 +11,52 @@ get_header(); ?>
 
 			<article id="post-0" class="post error404 not-found" role="article">
 				<header class="entry-header">
-					<h1 class="entry-title"><?php _e( 'This is somewhat embarrassing, isn&rsquo;t it?', 'themename' ); ?></h1>
+					<h1 class="entry-title"><?php _e( 'Oh dear...', 'themename' ); ?></h1>
 				</header>
 
 				<div class="entry-content">
-					<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching, or one of the links below, can help.', 'themename' ); ?></p>
+					<?php if (qtrans_getLanguage() == 'en') : ?>
+						<p>It seems we can&rsquo;t find what you&rsquo;re looking for. 
+						Perhaps searching, or one of the links below, can help you get away from this black hole of doom. 
+						Unless I fucked something up, in which case you're on your own(And you should let me know :)).</p>
+					<?php else : ?>
+						<p>Het ziet er naar uit dat we niet kunnen vinden wat je zoekt.
+						Wellicht dat zoeken, of een van de links hieronder, je kunnen helpen om uit dit zwarte gat van de ondergang te komen.
+						Tenzij ik iets stuk heb gemaakt, dan sta je er alleen voor(En je zou me dit moeten laten weten :))</p>
+					<?php endif; ?>
+				
+					<section>
+						<div id="search-archives">
+							<?php get_search_form(); ?>
+						</div>
+					</section>
+					
+					<section>
+						<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
+					</section>
+					
+					<section>
+						<div id="archive-months">
+							<?php the_widget( 'WP_Widget_Archives', 'show_post_count=true', "after_title=</h2>" ); ?>
+							<div class="clear"></div>
+						</div>						
+					</section>
 
-					<?php get_search_form(); ?>
-
-					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
-
-					<div class="widget">
-						<h2 class="widgettitle"><?php _e( 'Most Used Categories', 'themename' ); ?></h2>
-						<ul>
-						<?php wp_list_categories( array( 'orderby' => 'count', 'order' => 'DESC', 'show_count' => 'TRUE', 'title_li' => '', 'number' => '10' ) ); ?>
-						</ul>
-					</div>
-
-					<?php
-					$archive_content = '<p>' . sprintf( __( 'Try looking in the monthly archives. %1$s', 'themename' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-					?>
-
-					<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
+					<section>
+						<div id="archive-categories">
+							<h2 class="widgettitle"><?php _e( 'Most Used Categories', 'themename' ); ?></h2>
+							<ul>
+								<?php wp_list_categories( array( 'orderby' => 'count', 'order' => 'DESC', 'show_count' => 'TRUE', 'title_li' => '', 'number' => '10' ) ); ?>
+							</ul>
+							<div class="clear"></div>
+						</div>
+					</section>
+					
+					<section>
+						<div id="archive-tags">
+							<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
+						</div>				
+					</section>
 
 				</div><!-- .entry-content -->
 			</article><!-- #post-0 -->
