76e684b8d9adfcc4e32d3f72d4e24e761cfa5e16
Jeffrey de Wit <Jeffrey.deWit@gmail.com>
2012-08-16 16:04:04 -0400

    Noscript added for top nav

--- loop.php
+++ loop.php
@@ -7,10 +7,12 @@
 
 <?php /* Show page navigation when applicable */ ?>
 <?php if ( $wp_query->max_num_pages > 1 ) : ?>
-	<nav id="nav-above" role="article">
-		<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'cheffism' ) ); ?></div>
-		<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'cheffism' ) ); ?></div>
-	</nav><!-- #nav-above -->
+	<noscript>
+		<nav id="nav-above" role="article">
+			<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'cheffism' ) ); ?></div>
+			<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'cheffism' ) ); ?></div>
+		</nav><!-- #nav-above -->
+	</noscript>
 <?php endif; ?>
 
 <?php /* Start the Loop */  ?>
