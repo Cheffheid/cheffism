c77713591bc2e697156e2e4e89681032e2c907da
Jeffrey de Wit <Jeffrey.deWit@gmail.com>
2012-08-17 14:52:10 -0400

    Removed unnecessary function
    Caused errors. Forgot to remove it in the first place :D

--- functions.php
+++ functions.php
@@ -202,8 +202,8 @@ if (!current_user_can('manage_options')) {
 
 ?>
 <?php // asynchronous google analytics: mathiasbynens.be/notes/async-analytics-snippet
-//	 change the UA-XXXXX-X to be your site's ID
-/*add_action('wp_footer', 'async_google_analytics');
+//change the UA-XXXXX-X to be your site's ID
+add_action('wp_footer', 'async_google_analytics');
 function async_google_analytics() { ?>
 	<script>
 	var _gaq = [['_setAccount', 'UA-XXXXX-X'], ['_trackPageview']];
@@ -215,56 +215,7 @@ function async_google_analytics() { ?>
 			s.parentNode.insertBefore(g, s);
 		})(document, 'script');
 	</script>
-<?php }*/ ?>
-<?php /*
- * A default custom post type. DELETE from here to the end if you don't want any custom post types
- */
-/*add_action('init', 'create_boilertemplate_cpt');
-function create_boilertemplate_cpt() 
-{
-  $labels = array(
-    'name' => _x('HandcraftedWPTemplate CPT', 'post type general name'),
-    'singular_name' => _x('HandcraftedWPTemplate CPT Item', 'post type singular name'),
-    'add_new' => _x('Add New', 'handcraftedwptemplate_robot'),
-    'add_new_item' => __('Add New Item'),
-    'edit_item' => __('Edit Item'),
-    'new_item' => __('New Item'),
-    'view_item' => __('View Item'),
-    'search_items' => __('Search Items'),
-    'not_found' =>  __('No items found'),
-    'not_found_in_trash' => __('No items found in Trash'), 
-    'parent_item_colon' => ''
-  );
-  $args = array(
-    'labels' => $labels,
-    'public' => true,
-    'show_ui' => true, 
-    'query_var' => true,
-    'rewrite' => true,
-    'capability_type' => 'page',
-    'hierarchical' => false,
-    'menu_position' => 20,
-    'supports' => array('title','editor')
-  ); 
-  register_post_type('handcraftedwptemplate_robot',$args);
-}*/
-/*
- * This is for a custom icon with a colored hover state for your custom post types. You can download the custom icons here 
- http://randyjensenonline.com/thoughts/wordpress-custom-post-type-fugue-icons/
- */
-/*add_action( 'admin_head', 'cpt_icons' );
-function cpt_icons() {
-    ?>
-    <style type="text/css" media="screen">
-        #menu-posts-handcraftedwptemplaterobot .wp-menu-image {
-            background: url(<?php bloginfo('template_url') ?>/images/robot.png) no-repeat 6px -17px !important;
-        }
-		#menu-posts-handcraftedwptemplaterobot:hover .wp-menu-image, #menu-posts-handcraftedwptemplaterobot.wp-has-current-submenu .wp-menu-image {
-            background-position:6px 7px!important;
-        }
-    </style>
-<?php }*/ ?>
-
+<?php } ?>
 <?php
 
 /* Additional functions */
