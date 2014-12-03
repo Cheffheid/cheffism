fb83d009c9032b7f34784c236c0b30cbe3dd8ba8
Jeffrey de Wit <Nasgath@gmail.com>
2014-12-03 11:26:12 -0500

    Added HotJar snippet

--- functions.php
+++ functions.php
@@ -146,6 +146,27 @@ function cheffism_async_google_analytics() { ?>
 add_action('wp_head', 'cheffism_async_google_analytics');
 
 /**
+ * HotJar beta snippet
+ * TODO: Move to plugin
+ */
+function cheffism_hotjar_snippet() { ?>
+    <script async type="text/javascript">
+        (function(f,b,g){
+            var xo=g.prototype.open,xs=g.prototype.send,c;
+            f.hj=f.hj||function(){(f.hj.q=f.hj.q||[]).push(arguments)};
+            f._hjSettings={hjid:4319, hjsv:2};
+            function ls(){f.hj.documentHtml=b.documentElement.outerHTML;c=b.createElement("script");c.async=1;c.src="//static.hotjar.com/c/hotjar-4319.js?sv=2";b.getElementsByTagName("head")[0].appendChild(c);}
+            if(b.readyState==="interactive"||b.readyState==="complete"||b.readyState==="loaded"){ls();}else{if(b.addEventListener){b.addEventListener("DOMContentLoaded",ls,false);}}
+            if(!f._hjPlayback && b.addEventListener){
+                g.prototype.open=function(l,j,m,h,k){this._u=j;xo.call(this,l,j,m,h,k)};
+                g.prototype.send=function(e){var j=this;function h(){if(j.readyState===4){f.hj("_xhr",j._u,j.status,j.response)}}this.addEventListener("readystatechange",h,false);xs.call(this,e)};
+            }
+        })(window,document,window.XMLHttpRequest);
+    </script>
+<?php }
+add_action('wp_head', 'cheffism_hotjar_snippet');
+
+/**
  * Various functions
  */
 
