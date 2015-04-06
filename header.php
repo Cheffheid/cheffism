ad9db88bd957d62096a02e9e0b578b366b2c7ffd
Jeffrey de Wit <Nasgath@gmail.com>
2015-04-05 22:26:11 -0400

    Translateable Menu string

--- header.php
+++ header.php
@@ -43,7 +43,7 @@
 			<a href="/"><?php bloginfo( 'name' ); ?></a>
 		</h1>
 		<a href="#access" class="mobile-nav">
-			<span class="fa fa-bars"></span> <span>Menu</span>
+			<span class="fa fa-bars"></span> <span><?php echo __('Menu', 'cheffism'); ?></span>
 		</a>
 		<nav id="access" role="navigation" class="header-nav nav">
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'container_class' => 'nav' ) ); ?>
